ee60e0de1218a0b7f1ede7c22339a682b6264dc7
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-02-11 20:06:43 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayListImplementation list = new ArrayListImplementation(10);
+
+        ArrayList<Integer> arrayList = new ArrayList<>(10);
+
+        list.add(1);
+        list.add(8);
+        list.add(20);
+        list.add(67);
+
+        list.remove(2);
+        list.add(1, 888);
+        list.print();
     }
 }
